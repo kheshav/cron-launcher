648405d38854b9e71f6f2074036dc35d069de706
kheshav.sewnundun <kheshav.sewnundun@mns.mu>
2023-04-28 20:23:54 +0400

    Initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,20 @@
 use std::{process::Command, env};
 use base64::{Engine as _, engine::general_purpose};
+use ini::Ini;
 
 mod utils;
 
 fn main() {
+    let conf = Ini::load_from_file("conf/settings.ini").unwrap();
+    let general_section = conf.section(Some("General")).unwrap(); 
     utils::output_separator_start();
     let mut args: Vec<String> = env::args().collect();
     args.remove(0);
     let argument: String = args.join(" ");
     let unique_id: String = general_purpose::STANDARD_NO_PAD.encode(argument.as_bytes());
     utils::format_output(format!("Launching command: {}", argument).as_str());
-    let output = Command::new("bash")
+    println!("using shell: {}",general_section.get("shell").unwrap());
+    let output = Command::new(general_section.get("shell").unwrap_or_else(|| "bash"))
         .arg("-c")
         .arg(argument)
         .output()
